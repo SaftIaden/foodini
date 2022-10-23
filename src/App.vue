669913435f5a586dd2a46621cfba8cf0f1754aa6
Robert Baumgartner <robert.baumgartner@htlwienwest.at>
2022-10-23 09:54:23 +0200

    changed drawer code to script setup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+
+const leftDrawerOpen = ref(false);
+const rightDrawerOpen = ref(false);
+
+const toggleLeftDrawer = () => (leftDrawerOpen.value = !leftDrawerOpen.value);
+const toggleRightDrawer = () => (rightDrawerOpen.value = !rightDrawerOpen.value);
+</script>
+
 <template>
   <q-layout view="hHh lpR fFf">
-
-    <q-header  elevated class="bg-primary text-white" height-hint="98">
+    <q-header elevated class="bg-primary text-white" height-hint="98">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
           </q-avatar>
           Title
         </q-toolbar-title>
@@ -16,45 +25,21 @@
       </q-toolbar>
 
       <q-tabs align="left">
-        <q-route-tab to="/" label="Home"/>
+        <q-route-tab to="/" label="Home" />
         <q-route-tab to="/about" label="About" />
       </q-tabs>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" :breakpoint="500" side="left"  bordered>
+    <q-drawer v-model="leftDrawerOpen" side="left" bordered>
       <!-- drawer content -->
     </q-drawer>
 
-    <q-drawer v-model="rightDrawerOpen" :breakpoint="500" side="right"   bordered>
+    <q-drawer v-model="rightDrawerOpen" side="right" bordered>
       <!-- drawer content -->
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
-
-<script>
-import { ref } from 'vue'
-
-export default {
-  setup () {
-    const leftDrawerOpen = ref(false)
-    const rightDrawerOpen = ref(false)
-
-    return {
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      },
-
-      rightDrawerOpen,
-      toggleRightDrawer () {
-        rightDrawerOpen.value = !rightDrawerOpen.value
-      }
-    }
-  }
-}
-</script>
