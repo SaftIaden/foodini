1b7645b364371cd46c8262cb4296b173107e54ce
Robert Baumgartner <robert.baumgartner@htlwienwest.at>
2022-12-30 10:42:19 +0100

    changed store template

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import { Quasar } from 'quasar';
-import { createPinia } from 'pinia'
-import quasarIconSet from 'quasar/icon-set/fontawesome-v6';
+import { createPinia } from 'pinia';
 
 import '@quasar/extras/roboto-font/roboto-font.css';
 import '@quasar/extras/material-icons/material-icons.css';
-import '@quasar/extras/fontawesome-v6/fontawesome-v6.css';
+import quasarIconSet from 'quasar/icon-set/material-icons';
 
 import 'quasar/src/css/index.sass';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 app.use(Quasar, {
   plugins: {},
   iconSet: quasarIconSet,
 });
 
-app.mount('#app')
+app.mount('#app');
