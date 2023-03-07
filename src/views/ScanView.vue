2df9f21e1da7f07cdb6f51985f0c6e9be8d2ae51
TheoMeinhardt <meinhardt.t04@htlwienwest.at>
2023-03-07 10:59:02 +0100

    mirror barcode stream & style-tag set to scoped

--- src/views/ScanView.vue
+++ src/views/ScanView.vue
@@ -18,55 +18,48 @@ const store = useProductStore();
 const router = useRouter();
 
 const props = defineProps({
-	barcode: {
-		type: String,
-		required: true,
-	},
+  barcode: {
+    type: String,
+    required: true,
+  },
 });
 
-const onDecode = async (result) => {
-	runScanner.value = false;
-	console.log(result);
 
-	router.push(`add/${result}`);
-	/*window.navigator.vibrate(1000); */
+const onDecode = async (result) => {
+  runScanner.value = false;
+  console.log(result);
+
+  router.push(`add/${result}`);
+  /*window.navigator.vibrate(1000); */
 };
 const onLoaded = () => {
-	console.log('loaded');
+  console.log('loaded');
 };
+
 </script>
 <template>
-	<q-btn class="btn" @click="runScanner = !runScanner">SCAN</q-btn>
-	<div>
-		<div class="box"></div>
-		<div class="barcode-reader-container">
-			<StreamBarcodeReader v-if="runScanner" @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
-		</div>
-		<!-- <QrcodeScanner v-if="runScanner" :qrbox="250" :fps="10" @result="onScanResult"></QrcodeScanner> -->
-	</div>
+  <q-btn class="btn" @click="runScanner = !runScanner">SCAN</q-btn>
+  <div>
+    <div class="box"></div>
+    <StreamBarcodeReader v-if="runScanner" @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+    <!-- <QrcodeScanner v-if="runScanner" :qrbox="250" :fps="10" @result="onScanResult"></QrcodeScanner> -->
+  </div>
 </template>
-<style scoped>
+<style>
 .btn {
-	position: absolute;
-	bottom: calc(33.33% - 30px);
-	/* Position button at bottom third of screen, with some padding */
-	left: 50%;
-	/* Position button in the horizontal center of the container */
-	transform: translateX(-50%);
-	/* Move button back to the left by half its own width */
-	width: 90px;
-	/* Set button width */
-	height: 50px;
-	/* Set button height */
+  position: absolute;
+  bottom: calc(33.33% - 30px);
+  /* Position button at bottom third of screen, with some padding */
+  left: 50%;
+  /* Position button in the horizontal center of the container */
+  transform: translateX(-50%);
+  /* Move button back to the left by half its own width */
+  width: 90px;
+  /* Set button width */
+  height: 50px;
+  /* Set button height */
 }
-
-.barcode-reader-container {
-	display: flex;
-	justify-content: center;
-	transform: scaleX(-1); /* Mirror the video horizontally */
-}
-
-.box {
-	height: 200px;
+.box{
+  height: 200px;
 }
 </style>
