1bb70cb85793e2e80ec9c8881ecb55c5fadb53e7
KMatura <matura.k04@htlwienwest.at>
2023-03-13 20:45:26 +0100

    caching api calls

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,44 @@
-import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
-import vue from '@vitejs/plugin-vue';
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
-import manifest from './manifest';
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import vue from "@vitejs/plugin-vue";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+import manifest from "./manifest";
 
 export default defineConfig({
-  server: {
-    port: 8080,
-  },
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    VitePWA({ registerType: 'autoUpdate', manifest }),
-    quasar({
-      sassVariables: 'src/quasar-variables.sass',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
+	server: {
+		port: 8080,
+	},
+	plugins: [
+		vue({
+			template: { transformAssetUrls },
+		}),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest,
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+				runtimeCaching: [
+					{
+						urlPattern: new RegExp("^https://world.openfoodfacts.org/api/"),
+						handler: "CacheFirst",
+						options: {
+							cacheName: "open-food-facts-api-cache",
+							expiration: {
+								maxAgeSeconds: 60 * 60 * 24, // cache for 1 day
+							},
+						},
+					},
+				],
+			},
+		}),
+		quasar({
+			sassVariables: "src/quasar-variables.sass",
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
 });
